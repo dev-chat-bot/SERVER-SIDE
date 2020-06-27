7f606761c18ecd8ebfbfc27dce929db07a8e6a03
yodji09 <yodji09@gmail.com>
2020-06-28 00:48:52 +0700

    Add Handle error on Test Please

--- controller/userController.js
+++ controller/userController.js
@@ -10,6 +10,9 @@ class UserController{
             if(!user){
                 user = await User.findOne({email: username})
             }
+            if(user === null) {
+              throw Error('user does\'nt exit')
+            }
             let {_id, email} = user
             if(comparePassword(password, user.password)){
                 let access_token = generateToken({ _id, email, username: user.username })
@@ -18,7 +21,11 @@ class UserController{
                 res.status(400).json({error: 'password not match'})
             }
         } catch (error) {
+          if(error.message === 'user does\'nt exit') {
+            res.status(400).json({error: 'user does\'nt exit'})
+          } else {
             res.status(500).json({error})
+          }
         }
     }
 
@@ -36,20 +43,20 @@ class UserController{
             }
             const newUser = await user.save()
             let access_token = generateToken({_id: newUser._id, email: newUser.email, username: newUser.username})
-            res.status(201).json({access_token})            
+            res.status(201).json({access_token})
         } catch (error) {
             if(error.keyValue){
                 if(error.keyValue.email){
                     res.status(400).json({error: 'email already exists'})
                 } else if(error.keyValue.username){
                     res.status(400).json({error: 'username already exists'})
-                } 
+                }
             } else if(error.errors) {
                 if(error.errors.password){
                     const message = error.errors.password.properties.type
                     if(message == 'required'){
                         res.status(400).json({error: 'field must not be empty'})
-                    }  
+                    }
                     if(message == 'minlength'){
                         res.status(400).json({error: 'password length min have 8 characters'})
                     }
@@ -65,12 +72,12 @@ class UserController{
                     const message = error.errors.username.properties.type
                     if(message == 'required'){
                         res.status(400).json({error: 'field must not be empty'})
-                    }  
+                    }
                 } else if(error.errors.confirmPassword){
                     const message = error.errors.confirmPassword.properties.type
                     if(message == 'required'){
                         res.status(400).json({error: 'field must not be empty'})
-                    }  
+                    }
                     if(message == 'minlength'){
                         res.status(400).json({error: 'password length min have 8 characters'})
                     }
